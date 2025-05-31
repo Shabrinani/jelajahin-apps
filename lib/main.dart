b9844c4a08655a813796c79af1a71e6e38291892
brarida <aufaabrarida111@gmail.com>
2025-05-31 18:17:13 +0700

    font and add page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jelajahin_apps/pages/home.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/login.dart';
 
 void main() {
@@ -13,6 +14,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.grey[100],
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ).apply(
+          bodyColor: Colors.black87,
+          displayColor: Colors.black87,
+        ),
+      ),
       home: const LoginPage(),
     );
   }
